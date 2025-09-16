060df18790d7bdc593cd63917eb887da6d8b28ee
MunnaKumar32990 <2300032990@kluniversity.in>
2025-09-16 10:31:08 +0530

    added war path4

--- Frontend/employee-frontend/src/config.js
+++ Frontend/employee-frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  url: "http://localhost:8080/EmployeeManagement" // Adjust port number to match your backend
+  url: "http://localhost:2030/EmployeeManagement" // Adjust port number to match your backend
 };
 
 export default config;
