f9bc1a186dba98b15a6a8246e09884a5a443ef01
Munna Kumar <2300032990@kluniversity.in>
2025-09-22 12:34:32 +0530

    Update config.js

--- Frontend/employee-frontend/src/config.js
+++ Frontend/employee-frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
 
 
-  url: "http://localhost:2030" // Adjust port number to match your backend
+  url: "http://localhost:2030/EmployeeManagement" // Adjust port number to match your backend
 };
 
 export default config;
